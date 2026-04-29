a42dbf4d664f7002dfb44358f093ec2628f93169
abdul-korim-web <moneyschorsing@gmail.com>
2026-04-29 21:53:42 +0600

    Marquee section added

---
+++ src/Components/Marquee.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Marquee from "react-fast-marquee";
+
+const MarqueeSection = () => {
+  return (
+      <Marquee className="bg-black text-white py-2 italic font-mono ">
+         "At TilesHub, we don't just supply tiles; we provide the canvas for your lifestyle. Experience the perfect harmony of strength, style, and sophistication."
+      </Marquee>
+  );
+};
+
+export default MarqueeSection;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Hero from '@/Components/Hero';
+import MarqueeSection from '@/Components/Marquee';
 import React from 'react';
 
 const Page = () => {
   return (
     <div>
       <Hero/>
+      <MarqueeSection/>
     </div>
   );
 }
