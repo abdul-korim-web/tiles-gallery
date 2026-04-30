66e70323add2a17d02a38cd2b93282307ee01847
abdul-korim-web <moneyschorsing@gmail.com>
2026-04-30 21:17:21 +0600

    login auth added

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,94 +1,94 @@
-"use client"
-import { authClient } from '@/lib/auth-client';
-import { Button } from '@heroui/react';
-import Link from 'next/link';
-import { useRouter } from 'next/navigation';
-import React, { useState } from 'react';
-import { FcGoogle } from 'react-icons/fc';
-import { toast } from 'react-toastify';
+"use client";
+import { authClient } from "@/lib/auth-client";
+import { Button } from "@heroui/react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { FcGoogle } from "react-icons/fc";
+import { toast } from "react-toastify";
 
 const Register = () => {
-  const router = useRouter()
-  const [loading,setLoading] = useState(false)
-  const handelRegister=async(e)=>{
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const handelRegister = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const registerFormData = new FormData(e.target);
 
-    e.preventDefault()
-    setLoading(true)
-    const loginFormData = new FormData(e.target)
-
-    const userData = Object.fromEntries(loginFormData.entries())
-    console.log('formData', userData)
-    const {data,error} = await authClient.signUp.email({...userData,callbackURL:"/"})
+    const userData = Object.fromEntries(registerFormData.entries());
+    const { data, error } = await authClient.signUp.email({
+      ...userData,
+      callbackURL: "/login",
+    });
     if (data) {
-      toast.success("Thank you for signup")
-      router.push("/")
-    } else{
-      toast.error(error?.message)
+      toast.success("Thank you for signup");
+      router.push("/login");
+    } else {
+      toast.error(error?.message);
     }
-    setLoading(false)
-    
-
-  }
-    return (
-         <div className="min-h-screen flex items-center justify-center bg-gray-50 px-4">
-
+    setLoading(false);
+  };
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 px-4">
       <div className="w-full max-w-md bg-white border rounded-2xl p-6 shadow-sm">
         <h2 className="text-2xl font-semibold text-center mb-6">
           Create Account
         </h2>
         <form onSubmit={handelRegister} className="space-y-4">
-
           <input
-          name='name'
+            name="name"
             type="text"
             placeholder="Name"
             className="w-full px-4 py-2 border rounded-lg outline-none focus:ring-2 focus:ring-black"
           />
 
           <input
-          name='email'
+            name="email"
             type="email"
             placeholder="Email"
             className="w-full px-4 py-2 border rounded-lg outline-none focus:ring-2 focus:ring-black"
           />
 
           <input
-          name='image'
+            name="image"
             type="text"
             placeholder="Photo URL"
             className="w-full px-4 py-2 border rounded-lg outline-none focus:ring-2 focus:ring-black"
           />
 
           <input
-          name='password'
+            name="password"
             type="password"
             placeholder="Password"
             className="w-full px-4 py-2 border rounded-lg outline-none focus:ring-2 focus:ring-black"
           />
 
-          <button
+          <Button
             type="submit"
-            className={`w-full cursor-pointer  bg-black text-white py-2 rounded-lg hover:bg-gray-800 transition ${loading ? "cursor-not-allowed bg-gray-500 " :""}`}
+            className={`w-full cursor-pointer  bg-black text-white py-2 rounded-lg hover:bg-gray-800 transition ${loading ? "cursor-not-allowed bg-gray-500 " : ""}`}
           >
-            {loading ? "Register..." :"Register"}
-          </button>
+            {loading ? "Register..." : "Register"}
+          </Button>
         </form>
-        <div className="my-4 text-center text-sm text-gray-400">
-          OR
-        </div>
-         <Button variant="outline" className="w-full border py-2 rounded-lg flex items-center justify-center gap-2 hover:bg-gray-100 transition">
-                  <span><FcGoogle /></span> Continue with Google
-                </Button>
+        <div className="my-4 text-center text-sm text-gray-400">OR</div>
+        <Button
+          variant="outline"
+          className="w-full border py-2 rounded-lg flex items-center justify-center gap-2 hover:bg-gray-100 transition"
+        >
+          <span>
+            <FcGoogle />
+          </span>{" "}
+          Continue with Google
+        </Button>
         <p className="text-sm text-center mt-4 text-gray-600">
           Already have an account?{" "}
           <Link href="/login" className="text-black font-medium">
             Login
           </Link>
         </p>
-
       </div>
     </div>
-    );
-}
+  );
+};
 
 export default Register;
