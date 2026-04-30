4d97afa767b75a8c7f3bf05a6ebf6840126270fc
abdul-korim-web <moneyschorsing@gmail.com>
2026-04-30 14:32:46 +0600

    single tile page added

---
+++ src/app/tiles/[id]/page.jsx
@@ -0,0 +1,76 @@
+import { Button } from "@heroui/react";
+import Image from "next/image";
+const fetchTile = async(tileId)=>{
+    const res = await fetch(`https://tiles-gallery-server.onrender.com/product/${tileId}`)
+    const data = res.json()
+    return data
+}
+export default async function TileDetailsPage({params}) {
+
+    const {id} =  await params
+    const tile = await fetchTile(id)
+
+console.log('tile', tile)
+if (tile?.error) {
+    return(
+        <div className="text-center py-20 text-2xl md:text-4xl font-bold italic font-mono">
+            tile not found
+        </div>
+    )
+    
+}
+
+  return (
+    <div className="max-w-[1200px] mx-auto px-4 py-10 md:mt-20">
+      <div className="grid md:grid-cols-2 gap-8 items-start">
+        <div className="relative w-full h-[350px] md:h-[450px] rounded-xl overflow-hidden">
+          <Image
+            src={tile?.image || "https://cdn-icons-png.freepik.com/512/2748/2748558.png"}
+            alt={tile?.title || "image"}
+            fill
+            className="object-cover"
+          />
+        </div>
+
+        <div>
+          <h1 className="text-2xl md:text-3xl font-semibold mb-3">
+            {tile?.title}
+          </h1>
+          <p className="text-lg font-bold mb-4">
+            {tile?.currency} {tile?.price}
+          </p>
+          <p className="text-sm text-gray-600 mb-4">{tile?.description}</p>
+          <div className="text-sm space-y-2 mb-4">
+            <p>
+              <span className="font-medium">Material:</span> {tile?.material}
+            </p>
+            <p>
+              <span className="font-medium">Dimensions:</span> {tile?.dimensions}
+            </p>
+            <p>
+              <span className="font-medium">Category:</span> {tile?.category}
+            </p>
+          </div>
+          <div className="flex gap-2 mb-6">
+            <span className="text-xs px-3 py-1 bg-gray-100 rounded-full">
+              Minimalist
+            </span>
+            <span className="text-xs px-3 py-1 bg-gray-100 rounded-full">
+              Blue
+            </span>
+          </div>
+          <p
+            className={`text-sm mb-4 ${
+              tile.inStock ? "text-green-600" : "text-red-500"
+            }`}
+          >
+            {tile.inStock ? "In Stock" : "Out of Stock"}
+          </p>
+          <Button className="px-6 py-2 rounded-full bg-black text-white text-sm hover:bg-gray-800 ">
+            Order Now
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
