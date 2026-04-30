4d519f860d0cd774cd97a37d3944939c144e92ba
abdul-korim-web <moneyschorsing@gmail.com>
2026-05-01 05:29:12 +0600

    Bug

--- src/proxy.js
+++ src/proxy.js
@@ -1,20 +1,20 @@
-import { NextResponse } from "next/server";
-import { auth } from "@/lib/auth";
+// import { NextResponse } from "next/server";
+// import { auth } from "@/lib/auth";
 
-export async function proxy(request) {
-  const session = await auth.api.getSession({
-    headers: request.headers,
-  });
+// export async function proxy(request) {
+//   const session = await auth.api.getSession({
+//     headers: request.headers,
+//   });
 
-  const pathname = request.nextUrl.pathname;
+//   const pathname = request.nextUrl.pathname;
 
-  if (!session) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
+//   if (!session) {
+//     return NextResponse.redirect(new URL("/login", request.url));
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-export const config = {
-  matcher: ["/tiles","/tiles/:path", "/profile"],
-};
+// export const config = {
+//   matcher: ["/tiles","/tiles/:path", "/profile"],
+// };
